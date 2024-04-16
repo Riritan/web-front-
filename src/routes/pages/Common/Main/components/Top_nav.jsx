4e28928e5d375ce806e923f3efc9661cb4094cba
Riritan <2ulkiki0621@gmail.com>
2024-04-16 11:42:01 +0900

    feat: 홈페이지 초기 설정

---
+++ src/routes/pages/Common/Main/components/Top_nav.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import './Top_nav.css';
+
+const Top_nav = () => {
+
+    return (
+
+        <div className="Top_nav">
+            <div className="top_nav_container" style={"padding:0px 0.5rem;"}>
+                <div className="top_nav_wrap">
+                    <div className="top_nav_item"></div>
+                    <span className="Tiltle_name">
+                        <img src="/jooyajooya.png" alt="img" />
+                    </span>
+                </div>
+
+
+            </div>
+
+        </div>
+
+
+    )
+
+
+
+
+
+}
+
+
+
+export default Top_nav;
---
+++ src/routes/pages/Common/Main/components/register_banner_box.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './register_banner_box.css';
+
+const Register_banner_box = () => {
+    return (
+        <div className="register_banner_box">
+            지금 가입하고,
+            <span class="highlight">1만코인</span>
+            받으세요</div>
+    )
+
+
+
+
+}
+
+
+
+export default Register_banner_box;
